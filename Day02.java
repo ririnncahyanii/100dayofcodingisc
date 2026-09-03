50ff211d407d5ecddc8d5d35a698d0912f69c1c2
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-03 18:40:58 +0800

    Day02.java

--- Day02.java
+++ Day02.java
@@ -1,7 +1,7 @@
 public class day2{
   public static void main(String[] args) {
     System.out.println("nama:ririn");
-    System.out.prin("umur:  ");
+    System.out.print("umur:  ");
     System.out.printf("%d tahun", 19);
 
   }
