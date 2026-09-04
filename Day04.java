0af952bdf2fe17e1b97de2d5bd4b06d39c351245
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-05 06:56:55 +0800

    Day04.java

---
+++ Day04.java
@@ -0,0 +1,15 @@
+public class day4{
+  public static void main(String[] args) {
+   byte umur = 17;
+    System.out.println("umur:"+ umur);
+    // byte di gunakan untuk menyimpan bilangan kecil
+  short jumlah penduduk = 25000;
+    System.out.println("jumlah penduduk:" + jumlah penduduk);
+    // digunakan untuk menyimpan bilangan bulat yang lebih besar daripada byte
+  int jumlah siswa = 350;
+    System.out.println("jumlah siswa:" + jumlah siswa);
+    /* digunakan untuk menyimpan bilangan bulat yang umum digunakan
+     * int juga paling sering digunakan */
+  long jumlah uang = 5000000000L;
+    System.out.println("jumlah uang:"+ jumlah uang);
+    // digunakan untuk menyimpan bilangan bulat yang sangat besar
