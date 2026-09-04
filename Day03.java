17c5c73a678b0c717f42312ef4c50bdc64ca5cd8
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-04 10:29:34 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,12 @@
+public class main {
+  public static void main(String[] Arts) {
+    System.out.println("Nama:ririn\numur:19tahun");
+   // \n untuk pindah baris
+    System.out.println("alamat: \tpasangkayu");
+    // \t untuk membuat jarak
+    System.out.println("Dia berkata:\"Halo!\"");
+    // \" untuk menampilkan tanda kutip
+    System.out.println("c:\\java\\belajar") ;
+    // \\ untuk menampilkan garis miring kebalik 
+  }
+}
