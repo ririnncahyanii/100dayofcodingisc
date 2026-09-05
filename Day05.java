14bf657d15425134740a865a89260c99a16a2799
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-06 00:48:46 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,22 @@
+public class day5 {
+  public static void main(String[] args) {
+    double tinggi = 168.2;
+    double berat = 72.8;
+    System.out.println("tinggi:" + tinggi);
+    System.out.println("berat:" + berat);
+    /* doable digunakan untuk menyimpan angka yang memiliki koma/desimal
+      *seperti tb,bb,harga,nilai rata-rata, 
+      * dan hasil perhitungan matematika*/
+    float tinggi = 168.2f;
+    float nilai = 87.5f;
+    System.out.println("tinggi:" + tinggi);
+    System.out.println("nilai:" + nilai);
+    /* float digunakan untuk menyimpan angka pecahan
+     * tetapi ketelitiannya lebih rendah dari pada double
+     * knp harus ada huruf f di belakang angka karena jika 
+     * kamu tidak menambahkan f di belakang angka maka Java 
+     * akan menganggap sebagai double sehingga bisa muncul erorr*/
+
+  }
+}
+     
