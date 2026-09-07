aeea0120b49fd7e5f8b146feaab939eeba364f99
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-08 06:48:08 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,11 @@
+public class day7 {
+  public static void main(String[] args) {
+    String nama = "ririn";
+    String jurusan = "informatika";
+    /* String bukan termasuk tipe data primitif
+     * karena huruf S nya kapital dan menggunakan
+     * petik dua */
+     System.out.println("nama:" + nama);
+     System.out.println("jurusan:"+ jurusan);
+  }
+}
