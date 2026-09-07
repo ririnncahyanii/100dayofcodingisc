47815eddc2fa68ab3389acfbcad7a29478523960
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-07 16:09:23 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,13 @@
+public class day5 {
+  public static void main(String[]args) {
+   
+    char jenisKelamin = 'A';
+    System.out.println("jenisKelamin:" + jenisKelamin);
+    // char untuk menyimpan satu karakter
+    Boolean belumLulus = false;
+    System.out.println("belumLulus:" + belumLulus);
+    /* Boolean untuk menyimpan dua kemungkinan 
+      * true(benar),folse(salah)*/
+  }
+}
+  
