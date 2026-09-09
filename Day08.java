9739fb50468eca03566eb48644c09dc9b7589164
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-09 08:22:39 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,12 @@
+public class day8 {
+  publik static void main(String[]args) {
+    // 1.deklarasi
+  int nilai;
+    // 2.inisialisasi
+  nilai = 80;
+  System.out.println("nilai awal:" + nilai);
+    // 3.update variabel
+  nilai = 90;
+  System.out.println("nilai setelah update:" + nilai);
+  }
+}
