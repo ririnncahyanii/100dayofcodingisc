462b6896d9fef8f2a40b0ec0edfc48e3856107d4
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-10 15:04:33 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,20 @@
+public class day9 {
+  public static void main(String[]args) {
+    final double PI = 3,14;
+    // tidak bisa diubah
+    double jariJari = 7;
+    // bisa di ubah 
+    double luas = PI*jariJari*jariJari;
+    System.out.println("luas lingkaran = " + luas);
+    /* constanta(final variabel) di Java adalah 
+     * variabel yang nilainya tidak boleh diubah
+     * setelah di beri nilai*/
+    /* contohnya 
+      * nilai PI 
+      * jumlah hari dalam seminggu
+      * harga tetap
+      * batas maksimal
+      * nilai pajak tertentu*/
+  }
+}
+     
