03bf1aac35db5920785f255110123390e9ddf395
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-11 19:48:49 +0800

    Day10.java

--- Day10.java
+++ Day10.java
@@ -1,3 +1,23 @@
 public class day10 {
   public static void main(String[]args) {
+    byte angka1 = -128;// nilai minimal
+    byte angka2 = 127;// nilai maksimal
+    System.out.println(angka1);
+    System.out.println(angka2);
+    short angka1 = -32.768;// nilai minimal
+    short angka2 = 32767;// nilai maksimal
+    System.out.println(angka1);
+    System.out.println(angka2);
+    int angka1 = -2147483648;// nilai minimal
+    int angka2 = 2147483647;// nilai maksimal
+    System.out.println(angka1);
+    System.out.println(angka2);
+    long angka1 = -9.223.372.036.854.775.808;
+    // nilai minimal
+    long angka2 = 9.223.372.036.854.775.806;
+    // nilai maksimal 
+  }
+}
+    
+    
     
