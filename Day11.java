951410fc2678c758d09b5556e7354e08d5ab9481
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-12 22:39:31 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,5 +1,6 @@
- public class day11{
-  public static void main (String[]args) {
+ import Java.util.Scanner;
+  public class day11{
+   public static void main (String[]args) {
     Scanner sc = new Scanner (System.in);
 
   System.out.print(s:"nama:");
@@ -20,6 +21,8 @@
     short tahunMasuk = input.nextShort();
  System.out.print(s:"jumlahSodara:");
     byte jumlahSodara = input.nextByte();
-
+   
+   input.clause();
+   
   }
-}
+ }
