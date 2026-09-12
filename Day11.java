2c5db44d1871425fcdc7ead660f8cfc357879f61
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-12 22:30:15 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,3 +1,25 @@
-public class day11 {
-  public static void main(String[]args) {
-    
+ public class day11{
+  public static void main (String[]args) {
+    Scanner sc = new Scanner (System.in);
+
+  System.out.print(s:"nama:");
+    String nama = input.nextLine();
+  System.out.print(s:"umur:");
+    int umur = input.nextInt();
+  System.out.print(s:"tinggi Badan:");
+    double tinggi = input.nextDouble();
+  System.out.print(s:"berat Badan:");
+    float berat = input.nextFloat();
+  System.out.print(s:"jenisKelamin:");
+    char jenisKelamin = input.nextChar();
+  System.out.print(s:"status mahasiswa [true/false]:";
+    boolean status = input.nextBoolean();
+  System.out.print(s:"nomor Dana [long]:");
+    long nomor dana = input.nextLong();
+  System.out.print(s:"tahun Masuk:");
+    short tahunMasuk = input.nextShort();
+ System.out.print(s:"jumlahSodara:");
+    byte jumlahSodara = input.nextByte();
+
+  }
+}
