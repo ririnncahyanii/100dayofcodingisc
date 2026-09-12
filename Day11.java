9f244ef91ecbfdab2c6d9a505cb802efe2708581
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-12 23:36:12 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -4,25 +4,37 @@
     Scanner sc = new Scanner (System.in);
 
   System.out.print(s:"nama:");
-    String nama = input.nextLine();
+    String nama = sc.nextLine();
   System.out.print(s:"umur:");
-    int umur = input.nextInt();
-  System.out.print(s:"tinggi Badan:");
-    double tinggi = input.nextDouble();
-  System.out.print(s:"berat Badan:");
-    float berat = input.nextFloat();
+    int umur = sc.nextInt();
+  System.out.print(s:"tinggiBadan:");
+    double tinggi = sc.nextDouble();
+  System.out.print(s:"beratBadan:");
+    float berat = sc.nextFloat();
   System.out.print(s:"jenisKelamin:");
-    char jenisKelamin = input.nextChar();
-  System.out.print(s:"status mahasiswa [true/false]");
-    boolean status = input.nextBoolean();
-  System.out.print(s:"nomor Dana [long]:");
-    long nomor dana = input.nextLong();
-  System.out.print(s:"tahun Masuk:");
-    short tahunMasuk = input.nextShort();
+    char jenisKelamin = sc.nextChar().charAt(0);
+  System.out.print(s:"statusMahasiswa:");
+    boolean status = sc.nextBoolean();
+  System.out.print(s:"nomorDana:");
+    long nomor dana = sc.nextLong();
+  System.out.print(s:"tahunMasuk:");
+    short tahunMasuk = sc.nextShort();
  System.out.print(s:"jumlahSodara:");
-    byte jumlahSodara = input.nextByte();
-   
-   input.clause();
+    byte jumlahSodara = sc.nextByte();
+
+    System.out.println(\n=== DATA YANG DIMASUKKAN ===");
+
+    System.out.println("nama:" + nama);
+    System.out.println("umur:" + umur);
+    System.out.println("tinggiBadan:" + tinggi);
+    System.out.println("beratBadan:" + berat);
+    System.out.println("jenisKelamin:" + jenisKelamin);
+    System.out.println("statusMahasiswa:" + status);
+    System.out.println("nomorDana:" + nomor dana);
+    System.out.println("tahunMasuk:" + tahunMasuk);
+    System.out.println("jumlahSodara:" + jumlahSodara);
+    
+   input.close();
    
   }
  }
