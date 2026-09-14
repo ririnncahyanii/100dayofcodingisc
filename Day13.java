5921e38873c1d1967b137375206fdb338716eee5
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-14 19:14:52 +0700

    Day13.java

--- Day13.java
+++ Day13.java
@@ -1,6 +1,25 @@
 public class day13{
   public static void main(String[]args){
-    String nama 
+    String nama;
+    nama = "jefri";
+    System.out.println("nama:" + nama);
+    int umur;
+    umur = 22;
+    System.out.println("umur:" + umur);
+    nama = "jeff";
+    System.out.println("nama:" + nama);
+    umur = 18;
+    System.out.println("umur:" + umur):
+                       
+                       
+      
+      
+      
+
+
+    
+    
+    
   
 
 
