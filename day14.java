4e80d872c30522de904ecb6c7d6db7695b87962f
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-15 18:36:49 +0700

    day14.java

---
+++ day14.java
@@ -0,0 +1,8 @@
+public class day14 {
+  public static void main(String[]args){
+    int angka1 = 20;
+    int angka2 = 8;
+    int tambah = angka1 + angka2;
+    int kurang = angka1 - angka2;
+    System.out.println("penjumlahan =" + tambah);
+    System.out.println("pengurangan =" + kurang);
