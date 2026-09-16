820b2cef9cc4b7f3f12276021f43825bbea89107
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-16 19:17:51 +0700

    Day15.java

--- Day15.java
+++ Day15.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
-public class day15 {
+public class Day15 {
+  public static void main(String[]args) {
+    Scanner input = new Scanner(System.in);
+    
+import java.util.Scanner;
+public class Day15 {
   public static void main(String[]args) {
     Scanner input = new Scanner(System.in);
     int jumlahKotak = input.nextInt();
     int apelPerKotak = input.nextInt();
-    int jumlahTetangga = input.nextint();
+    int jumlahTetangga = input.nextInt();
     System.out.println("jumlah kotak apel:" + jumlahKotak);                 
     System.out.println("jumlah apel disetiap kotak:" + apelPerKotak);
     System.out.println("jumlah tetangga:" + jumlahTetangga);
 
     int totalApel = jumlahKotak * apelPerKotak;
-    int apelPerTetanggga = totalApel / jumlahTetangga;
+    int apelPerTetangga = totalApel / jumlahTetangga;
 
-    System.out.println("\n---HASIL PERHITUNGAN ---")
+    System.out.println("\n---HASIL PERHITUNGAN ---");
       System.out.println("Total seluruh apel pak Budi:" + totalApel);
-      System.out.println("setiap tetangga mendapatkan: " + apelperTetangga "buah");
+      System.out.println("setiap tetangga mendapatkan: " + apelperTetangga + "buah");
   }
 }
-    
   
     
