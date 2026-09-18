409cab670cbc7d2d43b8ac987dfc2e13550a7596
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-18 16:29:25 +0700

    Day17.java

--- Day17.java
+++ Day17.java
@@ -1,16 +1,19 @@
-public class App {
+public class Day17 {
+ 
     public static void main(String[] args) throws Exception {
     
-        int angka =10;
+       int angka = 10;
         System.out.println("nilai awal:"+ angka);
         angka += 5;
         System.out.println("setelah += 5:" + angka);
         angka -= 3;
         System.out.println("setelah -= 3:" + angka);
         angka *= 2;
+        System.out.println("setelah *=2:" + angka);
+        angka /= 4;
         System.out.println("setelah /=4:" + angka);
         angka %= 4;
-        System.out.println("setelah%=4:" + angka);
+        System.out.println("setalah %=4:" + angka);
 
     }
 }
