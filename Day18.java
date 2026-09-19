cc6bc2e5cd189572a3a0b23cc9d9bce58f302809
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-19 11:50:13 +0700

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,10 @@
+public class Day18 {
+
+public static void main(String[] args) {
+    int angka = 100;
+    double hasil = angka;
+    System.out.println("Nilai int:" + angka);
+    System.out.println("Nilai double:" + hasil);
+    
+}
+}
