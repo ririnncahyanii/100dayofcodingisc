51dc3e7bcbfca80bacf254c8690d802418d6b72c
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-21 13:19:31 +0700

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,35 @@
+public class Day20 {
+
+public static void main(String[] args) {
+    byte databyte = 10;
+    short datashort = 100;
+    int dataint = 1000;
+    long datalong = 10000L;
+    float datafloat = 10.5f;
+    double datadouble = 20.75;
+    char kodechar = 'A';
+    boolean databoolean = true;
+
+    String sbyte = String.valueOf(databyte);
+    String sshort =String.valueOf(datashort);
+    String sint = String.valueOf(dataint);
+    String slong = String.valueOf(datalong);
+    String sfloat = String.valueOf(datafloat);
+    String sdouble = String.valueOf(datadouble);
+    String schar = String.valueOf(kodechar);
+    String sboolean = String.valueOf(databoolean);
+
+    System.out.println("byte\t:" + sbyte);
+    System.out.println("short\t:" + sshort);
+    System.out.println("int\t:" + sint);
+    System.out.println("long\t:" + slong);
+    System.out.println("float\t:" + sfloat);
+    System.out.println("double\t:" + sdouble);
+    System.out.println("char\t:" + schar);
+    System.out.println("boolean\t:" + databoolean);
+
+
+  }
+}  
+
+   
