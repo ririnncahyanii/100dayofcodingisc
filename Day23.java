fef7c57f0574277c8af646a45557951c4a807580
ririnncahyanii <ririn15cahyani27@gmail.com>
2026-09-24 08:55:12 +0700

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) {
+        int sisi = 10;
+        int luas = sisi*sisi;
+        System.out.println("sisi\t\t:" + sisi);
+        System.out.println("luas persegi\t:" + luas);
+    }
+}
